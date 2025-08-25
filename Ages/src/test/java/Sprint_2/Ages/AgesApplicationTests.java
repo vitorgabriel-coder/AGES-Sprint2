23b36610a243a2aa18050e7d3a6d3fe68c0e370d
vitorgabriel-coder <vitor.collatto@edu.pucrs.br>
2025-08-25 16:06:45 -0300

    Initial commit: Configuração inicial do projeto Spring Boot

---
+++ Ages/src/test/java/Sprint_2/Ages/AgesApplicationTests.java
@@ -0,0 +1,13 @@
+package Sprint_2.Ages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
